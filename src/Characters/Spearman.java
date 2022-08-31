9143bf4a6c77a9968a2724a5d6874f950b667e85
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-08-31 22:21:47 +0500

    Added the Spearman class

--- src/Characters/Spearman.java
+++ src/Characters/Spearman.java
@@ -1,2 +1,26 @@
-package Characters;public class Spearman {
+package Characters;
+
+import java.util.Random;
+
+/** Класс Копейщик */
+public class Spearman extends BaseHero{
+
+    /**
+     * Конструктор создания персонажа.
+     * @param name Имя персонажа заданного пользователем.
+     */
+    public Spearman(String name) {
+        super.name = name;
+
+        super.attack = 4;
+        super.protection = 5;
+        super.shotsFired = 0;
+        super.damage = new int[]{1, 3, 0};
+        super.health = 10;
+        super.speed = 4;
+        super.delivery = false;
+        super.magic = false;
+
+        damage[2] = new Random().nextInt(damage[0],damage[1] + 1);
+    }
 }
