c516d59051d117a8b6f28c7fc6514e8b6738dbad
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-08-31 22:20:15 +0500

    Added the Sniper class

--- src/Characters/Sniper.java
+++ src/Characters/Sniper.java
@@ -1,2 +1,26 @@
-package Characters;public class Sniper {
+package Characters;
+
+import java.util.Random;
+
+/** Класс Снайпер*/
+public class Sniper extends BaseHero{
+
+    /**
+     * Конструктор создания персонажа.
+     * @param name Имя персонажа заданного пользователем.
+     */
+    public Sniper(String name) {
+        super.name = name;
+
+        super.attack = 12;
+        super.protection = 10;
+        super.shotsFired = 32;
+        super.damage = new int[]{8, 10, 0};
+        super.health = 15;
+        super.speed = 9;
+        super.delivery = false;
+        super.magic = false;
+
+        damage[2] = new Random().nextInt(damage[0],damage[1] + 1);
+    }
 }
