41aca96e71493d078d222af0b60110f67d830643
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-08-31 22:18:12 +0500

    Added the Monk class

--- src/Characters/Monk.java
+++ src/Characters/Monk.java
@@ -1,2 +1,26 @@
-package Characters;public class Monk {
+package Characters;
+
+import java.util.Random;
+
+/** Класс Монах */
+public class Monk extends BaseHero{
+
+    /**
+     * Конструктор создания персонажа.
+     * @param name Имя персонажа заданного пользователем.
+     */
+    public Monk(String name) {
+        super.name = name;
+
+        super.attack = 12;
+        super.protection = 7;
+        super.shotsFired = 0;
+        super.damage = new int[]{-4, -4, 0};
+        super.health = 30;
+        super.speed = 5;
+        super.delivery = false;
+        super.magic = true;
+
+        damage[2] = new Random().nextInt(damage[0],damage[1] + 1);
+    }
 }
