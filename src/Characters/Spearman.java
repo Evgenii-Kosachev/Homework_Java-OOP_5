910ea5fa29a9dea01b1f104254662f84dd596044
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-08 00:45:19 +0500

    The step method has been redefined.

--- src/Characters/Spearman.java
+++ src/Characters/Spearman.java
@@ -1,6 +1,7 @@
 package Characters;
 
 import java.util.List;
+import java.util.Random;
 
 /** Класс Копейщик */
 public class Spearman extends BaseHero{
@@ -12,17 +13,24 @@ public class Spearman extends BaseHero{
         this.attack = 4;
         this.defence = 5;
         this.shotsFired = 0;
-        this.damage = new Vector2(1.0F, 3.0F);
+        this.damage = new Vector2(1, 3);
         this.crntHeals = this.health = 10;
         this.speed = 4;
         this.delivery = false;
         this.magic = false;
         this.status = "stand";
-        this.position = new Vector2((float)x, (float)y);
+        this.position = new Vector2(x, y);
     }
 
     public boolean status() {return this.status.equals("active");}
 
     public void step() {
+        int hr = new Random().nextInt(list.size());
+        if(!list.get(hr).status.equals("Die.")) {
+            if(this.attack > list.get(hr).defence) {
+                list.get(hr).crntHeals -= damage.x;
+                this.status = String.format("(%d:-%s)", hr + 1, this.damage.x);
+            } else this.status = String.format("(%d:-%s)", hr + 1, 0);
+        } else this.status = "miss";
     }
 }
