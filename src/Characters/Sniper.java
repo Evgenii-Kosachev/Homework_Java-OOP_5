7b2ef92d1e93850907a50b045cba41e3f84f1d07
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-08 00:44:11 +0500

    The step method has been redefined.

--- src/Characters/Sniper.java
+++ src/Characters/Sniper.java
@@ -1,6 +1,7 @@
 package Characters;
 
 import java.util.List;
+import java.util.Random;
 
 /** Класс Снайпер*/
 public class Sniper extends BaseHero{
@@ -11,17 +12,28 @@ public class Sniper extends BaseHero{
         this.name = "Sniper";
         this.attack = 12;
         this.defence = 10;
-        this.shotsFired = 32;
-        this.damage = new Vector2(8.0F, 10.0F);
+        this.crntShotsFired = shotsFired = 32;
+        this.damage = new Vector2(8, 10);
         this.crntHeals = this.health = 15;
         this.speed = 9;
         this.delivery = false;
         this.magic = false;
         this.status = "stand";
-        this.position = new Vector2((float)x, (float)y);
+        this.position = new Vector2(x, y);
     }
 
     public boolean status() {return this.status.equals("active");}
 
-    public void step() {}
+    public void step() {
+        if(this.crntShotsFired > 0) {
+            int hr = new Random().nextInt(list.size());
+            if(!list.get(hr).status.equals("Die.")) {
+                if(this.attack > list.get(hr).defence) {
+                    list.get(hr).crntHeals -= damage.x;
+                    this.status = String.format("(%d:-%s)", hr + 1, this.damage.x);
+                } else this.status = String.format("(%d:-%s)", hr + 1, 0);
+            } else this.status = "miss";
+            this.crntShotsFired--;
+        }
+    }
 }
