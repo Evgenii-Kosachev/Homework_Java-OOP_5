34ae9b543b70a3a70823f5601f336144a87c95fd
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-08 00:41:43 +0500

    The step method has been redefined.

--- src/Characters/Brigand.java
+++ src/Characters/Brigand.java
@@ -1,6 +1,7 @@
 package Characters;
 
 import java.util.List;
+import java.util.Random;
 
 /** Класс Разбойник */
 public class Brigand extends BaseHero{
@@ -12,16 +13,24 @@ public class Brigand extends BaseHero{
         this.attack = 8;
         this.defence = 3;
         this.shotsFired = 0;
-        this.damage = new Vector2(2.0F, 4.0F);
+        this.damage = new Vector2(2, 4);
         this.crntHeals = health = 10;
         this.speed = 6;
         this.delivery = false;
         this.magic = false;
         this.status = "stand";
-        this.position = new Vector2((float)x, (float)y);
+        this.position = new Vector2(x, y);
     }
 
     public boolean status() {return status.equals("active");}
 
-    public void step() {}
+    public void step() {
+        int hr = new Random().nextInt(list.size());
+        if(!list.get(hr).status.equals("Die.")) {
+            if(this.attack > list.get(hr).defence) {
+                list.get(hr).crntHeals -= damage.x;
+                this.status = String.format("(%d:-%s)", hr + 1, this.damage.x);
+            } else this.status = String.format("(%d:-%s)", hr + 1, 0);
+        } else this.status = "miss";
+    }
 }
