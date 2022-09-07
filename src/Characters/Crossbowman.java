16bf4a6f18c71e3304973e9b9410a9bbcd01bc41
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-08 00:42:32 +0500

    The step method has been redefined.

--- src/Characters/Crossbowman.java
+++ src/Characters/Crossbowman.java
@@ -1,6 +1,7 @@
 package Characters;
 
 import java.util.List;
+import java.util.Random;
 
 /** Класс Арбалетчик */
 public class Crossbowman extends BaseHero{
@@ -11,18 +12,28 @@ public class Crossbowman extends BaseHero{
         this.name = "Crossbowman";
         this.attack = 6;
         this.defence = 3;
-        this.shotsFired = 16;
-        this.damage = new Vector2(2.0F, 3.0F);
+        this.crntShotsFired = shotsFired = 16;
+        this.damage = new Vector2(2, 3);
         this.crntHeals = health = 10;
         this.speed = 4;
         this.delivery = false;
         this.magic = false;
         this.status = "stand";
-        this.position = new Vector2((float)x, (float)y);
+        this.position = new Vector2(x, y);
     }
 
-    public boolean status() {return status.equals("active");}
+    public boolean status() {return this.status.equals("active");}
 
     public void step() {
+        if(this.crntShotsFired > 0) {
+            int hr = new Random().nextInt(list.size());
+            if (!list.get(hr).status.equals("Die.")) {
+                if (this.attack > list.get(hr).defence) {
+                    list.get(hr).crntHeals -= damage.x;
+                    this.status = String.format("(%d:-%s)", hr + 1, this.damage.x);
+                } else this.status = String.format("(%d:-%s)", hr + 1, 0);
+            } else this.status = "miss";
+            this.crntShotsFired--;
+        }
     }
 }
