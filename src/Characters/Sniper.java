147f17f03cb08d29c935c9b73e98da49a1543e78
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-13 19:53:52 +0500

    Added the ability to remotely attack and move

--- src/Characters/Sniper.java
+++ src/Characters/Sniper.java
@@ -1,7 +1,6 @@
 package Characters;
 
 import java.util.List;
-import java.util.Random;
 
 /** Класс Снайпер*/
 public class Sniper extends BaseHero{
@@ -11,29 +10,38 @@ public class Sniper extends BaseHero{
         super(side);
         this.name = "Sniper";
         this.attack = 12;
-        this.defence = 10;
-        this.crntShotsFired = shotsFired = 32;
-        this.damage = new Vector2(8, 10);
-        this.crntHeals = this.health = 15;
-        this.speed = 9;
+        this.defence = 5;
+        this.crntShotsFired = shotsFired = 14;
+        this.damage = new Vector2(1, 3);
+        this.crntHeals = this.health = 14;
+        this.speed = 1;
         this.delivery = false;
         this.magic = false;
         this.status = "stand";
         this.position = new Vector2(x, y);
+        this.distance = 3;
     }
 
     public boolean status() {return this.status.equals("active");}
 
     public void step() {
-        if(this.crntShotsFired > 0) {
-            int hr = new Random().nextInt(list.size());
-            if(!list.get(hr).status.equals("Die.")) {
-                if(this.attack > list.get(hr).defence) {
-                    list.get(hr).crntHeals -= damage.x;
-                    this.status = String.format("(%d:-%s)", hr + 1, this.damage.x);
-                } else this.status = String.format("(%d:-%s)", hr + 1, 0);
-            } else this.status = "miss";
+        Vector2 hero = getDistance(getList());
+        if (this.crntShotsFired > 0 && !list.get(hero.x).status.equals("Die.") && hero.y <= this.distance) {
+            if (this.attack > list.get(hero.x).defence) {
+                list.get(hero.x).crntHeals -= this.damage.x;
+                this.status = String.format("(%d:-%s)", hero.x + 1, this.damage.x);
+            } else this.status = String.format("(%d:-%s)", hero.x + 1, 0);
             this.crntShotsFired--;
+        } else {
+            if(this.position.y == 1 || this.crntShotsFired == 0) {
+                this.distance = 10;
+                this.position.y++;
+                this.status = "step";
+            }
+            if(this.position.y > 1) {
+                this.position.y--;
+                this.status = "step";
+            }
         }
     }
 }
