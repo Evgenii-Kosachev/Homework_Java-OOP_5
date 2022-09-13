72a2f1114becc5a6898ad03060dd70b6e1a8d165
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-13 19:56:28 +0500

    Added the ability to move

--- src/Characters/Spearman.java
+++ src/Characters/Spearman.java
@@ -1,7 +1,6 @@
 package Characters;
 
 import java.util.List;
-import java.util.Random;
 
 /** Класс Копейщик */
 public class Spearman extends BaseHero{
@@ -10,27 +9,38 @@ public class Spearman extends BaseHero{
     public Spearman(List<BaseHero> side, int x, int y) {
         super(side);
         this.name = "Spearman";
-        this.attack = 4;
-        this.defence = 5;
+        this.attack = 10;
+        this.defence = 9;
         this.shotsFired = 0;
-        this.damage = new Vector2(1, 3);
-        this.crntHeals = this.health = 10;
-        this.speed = 4;
+        this.damage = new Vector2(3, 4);
+        this.crntHeals = this.health = 18;
+        this.speed = 1;
         this.delivery = false;
         this.magic = false;
         this.status = "stand";
         this.position = new Vector2(x, y);
+        this.distance = 1;
     }
 
     public boolean status() {return this.status.equals("active");}
 
     public void step() {
-        int hr = new Random().nextInt(list.size());
-        if(!list.get(hr).status.equals("Die.")) {
-            if(this.attack > list.get(hr).defence) {
-                list.get(hr).crntHeals -= damage.x;
-                this.status = String.format("(%d:-%s)", hr + 1, this.damage.x);
-            } else this.status = String.format("(%d:-%s)", hr + 1, 0);
-        } else this.status = "miss";
+        Vector2 hero = getDistance(getList());
+        if (!list.get(hero.x).status.equals("Die.") && hero.y <= this.distance) {
+            if (this.attack > list.get(hero.x).defence) {
+                list.get(hero.x).crntHeals -= this.damage.x;
+                this.status = String.format("(%d:-%s)", hero.x + 1, this.damage.x);
+            } else this.status = String.format("(%d:-%s)", hero.x + 1, 0);
+        } else {
+            if(this.position.y == 1) {
+                this.distance = 10;
+                this.position.y++;
+                this.status = "step";
+            }
+            if(this.position.y > 1) {
+                this.position.y--;
+                this.status = "step";
+            }
+        }
     }
 }
