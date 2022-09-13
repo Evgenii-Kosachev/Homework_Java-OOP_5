e72b96e724560d7ff2cea537ddda4aba114d6e3d
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-09-13 19:40:09 +0500

    Added method getDistance

--- src/Characters/BaseHero.java
+++ src/Characters/BaseHero.java
@@ -18,6 +18,7 @@ public abstract class BaseHero implements Actions {
     protected String status;
     protected Vector2 position;
     protected List<BaseHero> list;
+    protected int distance;
 
 
     protected List<BaseHero> getList() {return this.list;}
@@ -29,11 +30,38 @@ public abstract class BaseHero implements Actions {
     @Override
     public String getInfo() {
         if(this.crntHeals > 0) {
-            return this.name + " H:" + this.crntHeals + " D:" + this.defence + " A:" + this.attack + " " + this.status;
+//            return this.name + " H:" + this.crntHeals + " D:" + this.defence + " A:" + this.attack + " " + this.status;
+            return "[x:" + this.position.x + ", y:" + this.position.y + "] " + this.name + " H:" + this.crntHeals + " "  + this.status;
         }
         else {
             this.status = "Die.";
             return String.format("<<< The %s is dead >>>", this.name);
         }
     }
+
+
+
+    public Vector2 getDistance(List<BaseHero> side){
+        int dist = Integer.MAX_VALUE;
+        int out = 0;
+
+        for (BaseHero baseHero : side) {
+            float dX = Math.abs(baseHero.position.x - position.x);
+            float dY = Math.abs(baseHero.position.y - position.y);
+            long tD = Math.round(Math.sqrt(dX*dX + dY*dY));
+            if (dist > tD) {
+                out = side.indexOf(baseHero);
+                dist = (int) tD;
+            }
+        }
+        return new Vector2(out, dist);
+    }
+
+
+
+
+
+
+
+
 }
